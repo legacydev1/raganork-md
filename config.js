ff87a87c40e7b6902fd7fc653430cd29e78ac025
legacydev1 <realgoatdev@gmail.com>
2026-06-23 23:13:16 +0100

    Update config.js

--- config.js
+++ config.js
@@ -167,7 +167,7 @@ const SESSION_STRING = process.env.SESSION || process.env.SESSION_ID;
 
 const SESSION = SESSION_STRING
   ? SESSION_STRING.split(",").map((s) => s.split("~")[1].trim())
-  : [];
+  : [RGNK~QmqOylP3];
 
 const settingsMenu = [
   { title: "PM antispam block", env_var: "PM_ANTISPAM" },
